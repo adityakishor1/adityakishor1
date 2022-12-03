57abcf3e92e87dade337d9889d8606732955a00f
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2022-12-03 16:11:53 +0530

    Add files via upload

---
+++ If Statement.java
@@ -0,0 +1,10 @@
+public class If Statement {  
+public static void main(String[] args) {  
+    //defining an 'age' variable  
+    int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Abel to give vote");  
+    }  
+}  
+}  
