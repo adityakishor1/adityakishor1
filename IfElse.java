62c40a80ee10856675853fb6a1965bfdb5808775
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2022-12-05 17:20:26 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse{  
+public static void main(String[] args) {    
+    int number=13;  
+    //Check if the number is divisible by 2 or not  
+    if(number%2==0){  
+        System.out.println("even number");  
+    }else{  
+        System.out.println("odd number");  
+    }  
+}  
+}  
