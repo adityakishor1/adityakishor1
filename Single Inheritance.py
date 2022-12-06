bc1d64561c1ae51e0724db4f8bc727bdaee3d5cf
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2022-12-06 14:54:10 +0530

    Add files via upload

---
+++ Single Inheritance.py
@@ -0,0 +1,10 @@
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+d = Dog()  
+d.bark()  
+d.speak()  
