ccf1765176086e3d419ffa8bf44d4f2450b1d510
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2022-12-07 22:24:50 +0530

    Add files via upload

---
+++ Multi Level Inheritance.py
@@ -0,0 +1,15 @@
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#The child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+#The child class Dogchild inherits another child class Dog  
+class DogChild(Dog):  
+    def eat(self):  
+        print("Eating bread...")  
+d = DogChild()  
+d.bark()  
+d.speak()  
+d.eat()  
