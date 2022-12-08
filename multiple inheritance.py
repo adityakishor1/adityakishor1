9366a568e04f11e17d0938c5d1336c102fabfc39
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2022-12-08 15:36:48 +0530

    Add files via upload

---
+++ multiple inheritance.py
@@ -0,0 +1,13 @@
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(d.Summation(10,20))  
+print(d.Multiplication(10,20))  
+print(d.Divide(10,20))  
